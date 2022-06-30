ad1d6d6201e26a61820af42b3d91def3a41519ff
Ahmed Abu Fayed <ahmedmahtech@gmail.com>
2022-06-30 10:35:40 +0300

    commit

--- lib/responsive_page.dart
+++ lib/responsive_page.dart
@@ -7,6 +7,36 @@ class ResponsiveScreen extends StatelessWidget{
   Widget build(BuildContext context) {
 
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(currentAccountPicture:CircleAvatar(child: Text("A"),),accountName: Text("Ahmed"), accountEmail: Text("ahmedmahtech@gmail.com")),
+            ListTile(
+
+              leading:Icon(Icons.home),
+              trailing:Icon(Icons.arrow_forward_ios),
+              subtitle:Text("Go to home"),
+              title:Text("Home"),
+            ),
+            ListTile(
+
+              leading:Icon(Icons.favorite),
+              trailing:Icon(Icons.arrow_forward_ios),
+              subtitle:Text("Go to favourite"),
+              title:Text("Favourite"),
+            ),
+            ListTile(
+
+              leading:Icon(Icons.person),
+              trailing:Icon(Icons.arrow_forward_ios),
+              subtitle:Text("Go to profile"),
+              title:Text("Profile"),
+            ),
+
+          ],
+        ),
+
+      ),
       appBar: AppBar(
         title: Text("Responsive Screen"),
       ),
