bdccdb92040eda9894377ad0dd0395acf9e27997
Ahmed Abu Fayed <ahmedmahtech@gmail.com>
2022-06-30 10:13:40 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:gsgflutterfirst/facebook_pageone.dart';
 import 'package:gsgflutterfirst/facebook_pagetwo.dart';
 import 'package:gsgflutterfirst/instagram_ui.dart';
+import 'package:gsgflutterfirst/responsive_page.dart';
 
 void main() {
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
 
     //  home:FacebookPostUI()));
-       home:(FacebookUI())));
+       home:(ResponsiveScreen())));
 
 }
 
